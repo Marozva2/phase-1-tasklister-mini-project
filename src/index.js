39167bb1be11b02f518e5839be7b8204476d01f7
Marozva <isabokefrankline@gmail.com>
2023-10-06 20:01:02 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handle(e.target.new_task_description.value)
+    form.reset()
+  })
 });
+
+function handle(to){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', toDelete)
+  btn.textContent = 'x'
+  p.textContent = `${to} `
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+}
+
+function toDelete(e){
+  e.target.parentNode.remove()
+}
